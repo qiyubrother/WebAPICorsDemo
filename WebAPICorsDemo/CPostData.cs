a2d8ef4b2775059a46be72dfd89a63fc7724d1ed
qiyubrother <13240137763@163.com>
2018-11-24 00:47:45 +0800

    添加项目文件。

---
+++ WebAPICorsDemo/CPostData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICorsDemo
+{
+    public class CPostData
+    {
+        public string ReservationNo { get; set; }
+        public string HotelId { get; set; }
+        public string Mobile { get; set; }
+    }
+}
---
+++ WebAPICorsDemo/Controllers/ProcessPostDataController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPICorsDemo.Controllers
+{
+    [EnableCors("AllowCors"), Route("api/[controller]")]
+    [ApiController]
+    public class ProcessPostDataController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "ProcessPostData-1", "ProcessPostData-2" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        [EnableCors("any")]
+        public void Post([FromBody] CPostData value)
+        {
+
+        }
+    }
+}
